6f4360b1d0455ef53e9a84af9189eea1bdb5a81f
Pablo <augustopablo620@gmail.com>
2026-09-24 12:17:46 -0300

    feat: connect orders and tables routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,6 +5,8 @@ import Recursos from '../pages/Recursos/Recursos.jsx'
 import Login from '../pages/Login/Login.jsx'
 import Register from '../pages/Register/Register.jsx'
 import Dashboard from '../pages/Dashboard/Dashboard.jsx'
+import Pedidos from '../pages/Pedidos/Pedidos.jsx'
+import Mesas from '../pages/Mesas/Mesas.jsx'
 import PlaceholderPage from '../pages/Placeholder/PlaceholderPage.jsx'
 
 export default function AppRoutes() {
@@ -18,11 +20,11 @@ export default function AppRoutes() {
       <Route path="/cadastro" element={<Register />} />
 
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/pedidos" element={<PlaceholderPage title="Pedidos" />} />
+      <Route path="/pedidos" element={<Pedidos />} />
       <Route path="/clientes" element={<PlaceholderPage title="Clientes" />} />
       <Route path="/financeiro" element={<PlaceholderPage title="Financeiro" />} />
       <Route path="/cardapio" element={<PlaceholderPage title="Cardápio" />} />
-      <Route path="/mesas" element={<PlaceholderPage title="Mesas" />} />
+      <Route path="/mesas" element={<Mesas />} />
       <Route path="/estoque" element={<PlaceholderPage title="Produtos / Estoque" />} />
 
       <Route path="*" element={<Navigate to="/" replace />} />

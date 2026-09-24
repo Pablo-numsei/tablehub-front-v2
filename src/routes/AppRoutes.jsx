2518ce0843c74d1aa22d1a3f32b675990c828bce
Pablo <augustopablo620@gmail.com>
2026-09-24 11:44:26 -0300

    chore: add routes

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,25 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home/Home.jsx'
+import Login from '../pages/Login/Login.jsx'
+import Register from '../pages/Register/Register.jsx'
+import PlaceholderPage from '../pages/Placeholder/PlaceholderPage.jsx'
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Register />} />
+
+      <Route path="/dashboard" element={<PlaceholderPage title="Dashboard" />} />
+      <Route path="/pedidos" element={<PlaceholderPage title="Pedidos" />} />
+      <Route path="/clientes" element={<PlaceholderPage title="Clientes" />} />
+      <Route path="/financeiro" element={<PlaceholderPage title="Financeiro" />} />
+      <Route path="/cardapio" element={<PlaceholderPage title="Cardápio" />} />
+      <Route path="/mesas" element={<PlaceholderPage title="Mesas" />} />
+      <Route path="/estoque" element={<PlaceholderPage title="Produtos / Estoque" />} />
+
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  )
+}
